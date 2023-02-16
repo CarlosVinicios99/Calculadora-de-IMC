2cfa03f3d3d8ea170e4c108bdd29ed0f9678ca91
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-02-16 02:46:54 -0300

    falhas resolvidas

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 function calcularIMC(altura, peso){
-    return peso / Math.pow(altura)
+    return peso / Math.pow(altura, 2)
 }
 
 function classificarIMC(imc, nome){
     if(imc < 18.5){
-        return `${nome} seu IMC é ${imc.toFixed(2)}\nVocê está abaixo do peso normal`
+        return `${nome} seu IMC é ${imc.toFixed(2)}<br>Você está abaixo do peso normal.`
     }
 
     else if(imc <= 24.9){
-        return `${nome} seu IMC é ${imc.toFixed(2)}\nVocê está com o peso normal`
+        return `${nome} seu IMC é ${imc.toFixed(2)}<br>Você está com o peso normal.`
     }
 
     else if(imc <= 29.9){
-        return `${nome} seu IMC é ${imc.toFixed(2)}\nVocê está com sobrepeso`
+        return `${nome} seu IMC é ${imc.toFixed(2)}<br>Você está com sobrepeso.`
     }
 
     else if(imc <= 34.9){
-        return `${nome} seu IMC é ${imc.toFixed(2)}\nVocê está Obesidade Grau 1`
+        return `${nome} seu IMC é ${imc.toFixed(2)}<br>Você está Obesidade Grau 1.`
     }
 
     else if(imc <= 39.9){
-        return `${nome} seu IMC é ${imc.toFixed(2)}\nVocê está com Obesidade Grau 2`
+        return `${nome} seu IMC é ${imc.toFixed(2)}<br>Você está com Obesidade Grau 2.`
     }
 
     else {
-        return `${nome} seu IMC é ${imc.toFixed(2)}\nVocê está com Obesidade Grau 3`
+        return `${nome} seu IMC é ${imc.toFixed(2)}<br>Você está com Obesidade Grau 3.`
     }
 }
 
 function exibirResultado(resultado){
-    document.querySelector("#descricao-resutado").innerText(resultado)
+    console.log(resultado)
+    document.querySelector("#resultado").innerHTML = resultado
 }
-
 
 const botaoDeCalcular = document.querySelector("#botao-calcular")
 
-botaoDeCalcular.addEventListener("onclick", e => {
-    e.preventDefault()
-
-    const nome = document.querySelector("#nome").nodeValue
-    const altura = document.querySelector("#altura").nodeValue
-    const peso = document.querySelector("#peso").nodeValue
+botaoDeCalcular.addEventListener("click", e => {
+    const nome = document.querySelector("#nome").value
+    const altura = document.querySelector("#altura").value
+    const peso = document.querySelector("#peso").value
 
     const imc = calcularIMC(altura, peso)
     resultado = classificarIMC(imc, nome)
